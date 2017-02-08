a0e049d1bdb086d4ba7b742b52522322b7e00c04
Matt Polichette <matt.polichette@spingo.com>
2017-02-08 13:23:47 -0700

    Bug: Fix entities reducer... non-merging reductions were at wrong level

--- src/reducers/createEntitiesReducer.js
+++ src/reducers/createEntitiesReducer.js
@@ -13,48 +13,52 @@ const mergeNewEntities = (state, action) => {
   return state
 }
 
+const reduceEntityType = (state = {}, action) => {
+  // Notes:
+  // The real magic happens w/ selectors and metadata.
+  // That why nothing happens to entities, for example, on delete request until its successful
+  // This allows for optional selections which give optimistic results
+  const { payload, meta } = action
+  const { resourceClientId, resourceId } = meta
+  switch (action.type) {
+    case actionTypes.CREATE_RESOURCE_REQUEST: {
+      // Create a temporary item under a _creating key
+      return putUnder(state, '_creating', resourceClientId, payload)
+    }
+    case actionTypes.CREATE_RESOURCE_SUCCESS:
+    case actionTypes.CREATE_RESOURCE_FAILURE: {
+      // Remove the temporary item
+      return popUnder(state, resourceClientId, '_creating', false)
+    }
+
+    case actionTypes.UPDATE_RESOURCE_REQUEST:
+      // Place payload resource into _updating
+      return putUnder(state, '_updating', resourceId, payload)
+    case actionTypes.UPDATE_RESOURCE_SUCCESS: // Replacment by mergeNewEntities
+    case actionTypes.UPDATE_RESOURCE_FAILURE:
+      // Remove entity from _updating (no replacing)
+      return popUnder(state, resourceId, '_updating', false)
+
+    case actionTypes.DELETE_RESOURCE_SUCCESS: {
+      // Delete the item from entities
+      const newState = { ...state }
+      delete newState[resourceId]
+      return newState
+    }
+    default:
+      return state
+  }
+}
+
 export default (schemas) => {
   const initalState = initialStateFromSchemas(schemas)
   return (state = initalState, action) => {
-    const { payload, meta } = action
-    if (!(meta && meta.schema)) { return state }
-
+    if (!(action.meta && action.meta.schema)) { return state }
+    const { meta: { schema } } = action
     const newState = mergeNewEntities(state, action)
-
-    const { resourceClientId, resourceId } = meta
-
-    // Notes:
-    // The real magic happens w/ selectors and metadata.
-    // That why nothing happens to entities, for example, on delete request until its successful
-    // This allows for optional selections which give optimistic results
-    switch (action.type) {
-      case actionTypes.CREATE_RESOURCE_REQUEST: {
-        // Create a temporary item under a _creating key
-        return putUnder(state, '_creating', resourceClientId, payload)
-      }
-      case actionTypes.CREATE_RESOURCE_SUCCESS:
-      case actionTypes.CREATE_RESOURCE_FAILURE: {
-        // Remove the temporary item
-        return popUnder(state, resourceClientId, '_creating', false)
-      }
-
-      case actionTypes.UPDATE_RESOURCE_REQUEST:
-        // Place payload resource into _updating
-        return putUnder(state, '_updating', resourceId, payload)
-      case actionTypes.UPDATE_RESOURCE_SUCCESS:
-        // Replace entity w/ item from _updating
-        return popUnder(state, resourceId, '_updating')
-      case actionTypes.UPDATE_RESOURCE_FAILURE:
-        // Just remove entity from _updating (no replacing)
-        return popUnder(state, resourceId, '_updating', false)
-
-      case actionTypes.DELETE_RESOURCE_SUCCESS: {
-        // Delete the item from entities
-        delete newState[resourceId]
-        return newState
-      }
-      default:
-        return newState
+    return {
+      ...newState,
+      [schema]: reduceEntityType(newState[schema], action),
     }
   }
 }
