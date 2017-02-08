f657d043a7bd614e58c9410a529e5de90ea13cb8
Matt Polichette <matt.polichette@spingo.com>
2017-02-08 13:23:14 -0700

    Chore: Fix some missnamed functions

--- src/selectors/getPageEntities.js
+++ src/selectors/getPageEntities.js
@@ -2,11 +2,11 @@ import { denormalize } from 'normalizr'
 import getCruduxState from '../util/getCruduxState'
 import getPage from './getPage'
 
-const getEntity = (getCrudState = getCruduxState) =>
+const getPageEntities = (getCrudState = getCruduxState) =>
   (schema, pageKey, state) => {
     const { entities } = getCrudState(state)
     const page = getPage(getCrudState)(pageKey, state)
     return denormalize(page, [schema], entities)
   }
 
-export default getEntity
+export default getPageEntities
--- src/selectors/getPageGroupEntities.js
+++ src/selectors/getPageGroupEntities.js
@@ -2,10 +2,10 @@ import getCruduxState from '../util/getCruduxState'
 import getPageGroupPageKey from './getPageGroupPageKey'
 import getPageEntities from './getPageEntities'
 
-const getPageGroupPage = (getCrudState = getCruduxState) =>
+const getPageGroupEntities = (getCrudState = getCruduxState) =>
   (schema, pageGroup, state) => {
     const pageKey = getPageGroupPageKey(getCrudState)(pageGroup, state)
     return getPageEntities(getCrudState)(schema, pageKey, state)
   }
 
-export default getPageGroupPage
+export default getPageGroupEntities
